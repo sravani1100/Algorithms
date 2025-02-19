8f5810ba3efdc96df94491fdbbccd6a87939886f
sravani <sravanigangireddy11@gmail.com>
2025-02-19 16:51:22 +0530

    bit

---
+++ AlgorithmsJava/BitManipulations/src/PowerOf2.java
@@ -0,0 +1,8 @@
+public class PowerOf2 {
+    public static long pow2(int n){
+        return (long)1<<n;
+    }
+    public static void main(String[] args) {
+        System.out.println(pow2(31));
+    }
+}
