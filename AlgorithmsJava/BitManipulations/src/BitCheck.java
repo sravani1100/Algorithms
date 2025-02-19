3fd1eb220499a8569c12278144d6ecd89e49854f
sravani <sravanigangireddy11@gmail.com>
2025-02-19 20:30:31 +0530

    bit

--- AlgorithmsJava/BitManipulations/src/BitCheck.java
+++ AlgorithmsJava/BitManipulations/src/BitCheck.java
@@ -1,13 +1,24 @@
 public class BitCheck {
-    int cnt = 0;
-    public  int check(int N){
+   /* int cnt = 0;
+    public  int checkMethod1(int N){
         while(N != 0){
             N = N & (N-1);
             cnt++;
         }
         return cnt;
+    } */
+    public int checkMethod2(int N){
+        int cnt = 0;
+        while(N != 0){
+            if((N & 1) == 1){
+                cnt++;
+            }
+            N = N >> 1;
+        }
+        return cnt;
     }
     public static void main(String[] args) {
-        System.out.println(new BitCheck().check(15));
+        //System.out.println(new BitCheck().checkMethod1(15));
+        System.out.println(new BitCheck().checkMethod2(14));
     }
 }
