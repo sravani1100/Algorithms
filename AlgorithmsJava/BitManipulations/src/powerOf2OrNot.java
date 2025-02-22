591a0916f87f18c852126980d95a72f1d9840a08
sravani <sravanigangireddy11@gmail.com>
2025-02-22 22:07:04 +0530

    power2

--- AlgorithmsJava/BitManipulations/src/powerOf2OrNot.java
+++ AlgorithmsJava/BitManipulations/src/powerOf2OrNot.java
@@ -10,17 +10,25 @@ public class powerOf2OrNot {
     } */
 
     // Method 2
-    public boolean checkPowerMethod2(int N){
-        int cnt = 0;
-        while(N != 0) {
+    /*public boolean checkPowerMethod2(int N){
+       N = N & (N-1);
+       return (N == 0);
+    }*/
+
+    //Method 3
+    public boolean checkPowerMethod3(int N){
+        int cnt = 0 ;
+        while(N != 0){
             cnt++;
             N = N & (N - 1);
         }
         return (cnt == 1);
     }
 
+
     public static void main(String[] args) {
         //System.out.println(new powerOf2OrNot().checkPowerMethod1(256)); // true
-        System.out.println(new powerOf2OrNot().checkPowerMethod2(63)); // false
+        //System.out.println(new   powerOf2OrNot().checkPowerMethod2(63)); // false
+        System.out.println(new   powerOf2OrNot().checkPowerMethod3(33)); // false
     }
 }
