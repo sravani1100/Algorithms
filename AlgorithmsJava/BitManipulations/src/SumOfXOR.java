28764d6ca40dfd73b98721b3043877c4902ffe2e
sravani <sravanigangireddy11@gmail.com>
2025-04-10 21:27:02 +0530

    SumOfXOR

---
+++ AlgorithmsJava/BitManipulations/src/SumOfXOR.java
@@ -0,0 +1,19 @@
+public class SumOfXOR {
+    public int sum(int[] arr, int n){
+        int ans = 0;
+        for(int bit = 0; bit <= 30; bit++){
+            int cnt = 0;
+            for(int i = 0; i < n; i++){
+                if(((arr[i] >> bit) & 1) == 1)
+                    cnt++;
+            }
+            ans = ans + cnt * (n - cnt) * (1 << bit);
+        }
+        return 2 * ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 4, 18, 3};
+        int n = arr.length;
+        System.out.println(new SumOfXOR().sum(arr, n)); // 186
+        }
+}
